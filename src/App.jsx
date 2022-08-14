c34695b63502fe33067362c5cd523a7da75d0de4
andrei-sch <andrei.schrenck@gmail.com>
2022-08-14 17:32:56 +0300

    added Nav and Footer empty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Footer from "./components/Footer/Footer";
+import Nav from "./components/Nav/Nav";
 
 const App = () => {
   return (
     <div>
       <p>Your cleaned App component</p>
+      <Nav />
+      <Footer />
     </div>
   );
 };
