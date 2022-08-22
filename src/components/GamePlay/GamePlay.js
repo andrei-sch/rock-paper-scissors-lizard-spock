7a763aa8dac60dfad07e3a3781759a23343f0576
andrei-sch <andrei.schrenck@gmail.com>
2022-08-22 20:48:14 +0300

    moved the images to public folder

--- src/components/GamePlay/GamePlay.js
+++ src/components/GamePlay/GamePlay.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './GamePlay.css'
-import rock from "../../Assets/rock.svg"
+
 import rockcircle from "../../Assets/rock-circle.svg"
-// import paper from "../../Assets/paper.svg"
 import papercircle from "../../Assets/paper-circle.svg"
-// import scissors from "../../Assets/scissors.svg"
 import scissorscircle from "../../Assets/scissors-circle.svg"
-// import lizard from "../../Assets/lizard.svg"
 import lizardcircle from "../../Assets/lizard-circle.svg"
-// import spock from "../../Assets/spock.svg"
 import spockcircle from "../../Assets/spock-circle.svg"
 
 function GamePlay() {
+
+    const [turnsLeft, setTurnsLeft] = useState(10)
+    const [yourCurrentScore, setYourCurrentScore] = useState(0)
+    const [computerCurrentScore, setComputerCurrentScore] = useState(0)
+    const [turnMessage, setTurnMessage] = useState("paper covers rock")
+    const [yourChoice, setYourChoice] = useState("rock")
+    const [computerChoice, setComputerChoice] = useState("rock")
+    const [gameOver, setGameOver] = useState(false)
+
   return (
     <section className='game-container'>
-        <div className="turns-left"><p>turns left: 7</p></div>
+        <div className="turns-left"><p>turns left: {turnsLeft}</p></div>
         <div className="current-score">
-            <span id="my-current-score">3</span>
+            <span id="my-current-score">{yourCurrentScore}</span>
             <span id="colon-current-score">:</span>
-            <span id="computer-current-score">1</span>
+            <span id="computer-current-score">{computerCurrentScore}</span>
         </div>
-        <div className="in-game-turn-message"><p>paper covers rock</p></div>
+        <div className="in-game-turn-message"><p>{turnMessage}</p></div>
         <div className="chosen-options-wrapper">
             <div className="my-chosen-sign">
                 <span className="my-chosen-sign-text">you</span>
-                <img src={rock} alt="rock" className="my-chosen-sign-image" />
+                <img src={`../images/${yourChoice}.svg`} alt={yourChoice} className="my-chosen-sign-image" />
             </div>
             <div className="computer-chosen-sign">
-                <img src={rock} alt="rock" className="computer-chosen-sign-image" />
+                <img src={`../images/${computerChoice}.svg`} alt={computerChoice} className="computer-chosen-sign-image" />
                 <span className="computer-chosen-sign-text">computer</span>
             </div>
         </div>
