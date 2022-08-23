8ff61919a4274e77ede66dce75a0f44f68068086
andrei-sch <andrei.schrenck@gmail.com>
2022-08-23 12:02:11 +0300

    ceated the logic for the bottom buttons

--- src/components/GamePlay/GamePlay.js
+++ src/components/GamePlay/GamePlay.js
@@ -1,54 +1,77 @@
-import React, { useState } from 'react'
-import ImageGetter from '../ImageGetter/ImageGetter'
-import './GamePlay.css'
+import React, { useState } from "react";
+import ImageGetter from "../ImageGetter/ImageGetter";
+import ImageGetterBtn from "../ImageGetter/ImageGetterBtn";
+import "./GamePlay.css";
 
 function GamePlay() {
+  const [turnsLeft, setTurnsLeft] = useState(10);
+  const [yourCurrentScore, setYourCurrentScore] = useState(0);
+  const [computerCurrentScore, setComputerCurrentScore] = useState(0);
+  const [turnMessage, setTurnMessage] = useState("paper covers rock");
+  const [yourChoice, setYourChoice] = useState("rock");
+  const [computerChoice, setComputerChoice] = useState("rock");
+  const [gameOver, setGameOver] = useState(false);
 
-    const [turnsLeft, setTurnsLeft] = useState(10)
-    const [yourCurrentScore, setYourCurrentScore] = useState(0)
-    const [computerCurrentScore, setComputerCurrentScore] = useState(0)
-    const [turnMessage, setTurnMessage] = useState("paper covers rock")
-    const [yourChoice, setYourChoice] = useState("rock")
-    const [computerChoice, setComputerChoice] = useState("rock")
-    const [gameOver, setGameOver] = useState(false)
+  const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
-    const choices = ['rock','paper','scissors','lizard','spock']
+  const comboPick = yourChoice + computerChoice
+
+  const handleClickOption = (choice) => {
+    console.log(choice)
+  }
 
 
 
   return (
-    <section className='game-container'>
-        <div className="turns-left"><p>turns left: {turnsLeft}</p></div>
-        <div className="current-score">
-            <span id="my-current-score">{yourCurrentScore}</span>
-            <span id="colon-current-score">:</span>
-            <span id="computer-current-score">{computerCurrentScore}</span>
+    <section className="game-container">
+      <div className="turns-left">
+        <p>turns left: {turnsLeft}</p>
+      </div>
+      <div className="current-score">
+        <span id="my-current-score">{yourCurrentScore}</span>
+        <span id="colon-current-score">:</span>
+        <span id="computer-current-score">{computerCurrentScore}</span>
+      </div>
+      <div className="in-game-turn-message">
+        <p>{turnMessage}</p>
+      </div>
+      <div className="chosen-options-wrapper">
+        <div className="my-chosen-sign">
+          <span className="my-chosen-sign-text">you</span>
+          <ImageGetter
+            name={`${yourChoice}`}
+            classStyle={"my-chosen-sign-image"}
+          />
         </div>
-        <div className="in-game-turn-message"><p>{turnMessage}</p></div>
-        <div className="chosen-options-wrapper">
-            <div className="my-chosen-sign">
-                <span className="my-chosen-sign-text">you</span>
-                <ImageGetter name={`${yourChoice}`} classStyle={'my-chosen-sign-image'} />
-            </div>
-            <div className="computer-chosen-sign">
-                <ImageGetter name={`${computerChoice}`} classStyle={'computer-chosen-sign-image'} />
-                <span className="computer-chosen-sign-text">computer</span>
-            </div>
+        <div className="computer-chosen-sign">
+          <ImageGetter
+            name={`${computerChoice}`}
+            classStyle={"computer-chosen-sign-image"}
+          />
+          <span className="computer-chosen-sign-text">computer</span>
         </div>
-        <div className="pick-an-option-buttons">
+      </div>
+      <div className="pick-an-option-buttons">
+        {choices.map((choice,index) => {
+          return (
+            <ImageGetterBtn
+              name={`${choice}-circle`}
+              classStyle={"img-circle-options"}
+              classStyleBtn={"img-circle-options-btn"}
+              key={index}
+              handleClick={()=>handleClickOption(choice)}
+            />
+          );
+        })}
 
-            {choices.map(choice => {
-                return <ImageGetter name={`${choice}-circle`} classStyle={'img-circle-options'} />
-            })}
-
-            {/* <img src={rockcircle} alt="rockcircle" className='img-circle-options' />
+        {/* <img src={rockcircle} alt="rockcircle" className='img-circle-options' />
             <img src={papercircle} alt="papercircle" className='img-circle-options' />
             <img src={scissorscircle} alt="scissorscircle" className='img-circle-options' />
             <img src={lizardcircle} alt="lizardcircle" className='img-circle-options' />
             <img src={spockcircle} alt="spockcircle" className='img-circle-options' /> */}
-        </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default GamePlay
+export default GamePlay;
---
+++ src/components/ImageGetter/ImageGetterBtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ImageGetter({ name, classStyle,classStyleBtn, handleClick }) {
+  //import image
+  const image = require(`../../Assets/${name}.svg`);
+  if (!image) console.log(`Image ${name} doesn't exist`);
+  return (
+    <button className={classStyleBtn} onClick={()=>handleClick()}>
+      <img src={image} alt={name} className={classStyle} />
+    </button>
+  );
+}
+
+export default ImageGetter;
