a798b8e3feebb51ce82e6e24078250fc69de6420
andrei-sch <andrei.schrenck@gmail.com>
2022-08-30 02:14:14 +0300

    display bugs again..

--- src/components/Modals/RulzModal/RulzModal.js
+++ src/components/Modals/RulzModal/RulzModal.js
@@ -11,7 +11,7 @@ function RulzModal({open,onClose}) {
         <div className='overlay-for-rulz-modal' onClick={onClose}></div>
         <div className='rulz-popup-wrapper'>
             <p className="close-modal-popup" onClick={onClose}>X</p>
-            <img src={rulez1} alt="Game rules" />
+            <img src={rulez1} alt="Game rules" onClick={onClose} id="rulz-img"/>
         </div>
     </>
     
